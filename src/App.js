f233f064be0d14c2270976f2879e0307c752138c
VosKoen <Koen82@gmail.com>
2018-09-29 09:36:10 +0200

    step 1 completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const data = {
+  "Ivel Z3": {
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  "Bally Astrocade": {
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  "Sord M200 Smart Home Computer": {
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  "Commodore 64": {
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+};
+
+const pcModels = Object.keys(data);
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-      </div>
-    );
+    if (!pcModels)
+      return (
+        <div className="App">
+          <p>Loading...</p>
+        </div>
+      );
+
+    if (pcModels)
+      return (
+        <div className="App">
+          <select>
+            <option value="">-- pick a model --</option>
+            {pcModels.map(model => <option value={model}>{model} ({data[model].year})</option>)}
+          </select>
+        </div>
+      );
   }
 }
 
